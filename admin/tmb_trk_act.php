9737bf74951cbe794328433cd5d08bea7badbbbe
koswara2017 <34657705+koswara2017@users.noreply.github.com>
2018-05-31 01:40:39 +0700

    truk

--- admin/tmb_trk_act.php
+++ admin/tmb_trk_act.php
@@ -1,18 +1,21 @@
-<?php 
+<?php
 include 'config.php';
 $id=$_POST['id'];
 $nm_truk=$_POST['nm_truk'];
 $nopol=$_POST['nopol'];
 $nm_supir=$_POST['nm_supir'];
-
+$biaya=null;
+$pendapatan=null;
+$laba_rugi=null;
+$pm=null;
 
 if($_POST['submit']=='Simpan'){
-	mysql_query("insert into truk values('','$nm_truk','$nopol','$nm_supir')");
+	mysql_query("insert into truk values('','$nm_truk','$nopol','$nm_supir','$biaya','$pendapatan','$laba_rugi,'$pm')");
 	header("location:truck.php");
 }
 
 if($_POST['submit']=='Update'){
-	mysql_query("update truk set nm_truk='$nm_truk', nopol='$nopol', nm_supir='$nm_supir' where id='$id'");
+	mysql_query("update truk set nm_truk='$nm_truk', nopol='$nopol', nm_supir='$nm_supir', nm_supir='$nm_supir' where id='$id'");
 	header("location:truck.php");
 }
  ?>
---
+++ hapus_laporan_truk.php
@@ -0,0 +1,7 @@
+<?php
+include 'config.php';
+$id=$_GET['id'];
+mysql_query("delete from spk where id='$id'");
+header("location:spk.php?spk=1");
+
+?>
