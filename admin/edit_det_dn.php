590acbe287297b21f9faed587367ae0fcd50c413
and703 <andwishyou@gmail.com>
2018-06-01 21:16:50 +0700

    fix edit det dn

--- admin/edit_det_dn.php
+++ admin/edit_det_dn.php
@@ -20,23 +20,23 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="white-box p-l-20 p-r-20">
-                        <h3 class="box-title m-b-0"><a href="detail_dn.php?id=<?=$no_spk?>&cus=<?=$cu?>" class="btn btn-alt btn-sm btn-default toggle-bordered enable-tooltip" title="Kembali">Kembali</a></h3>
+                        <h3 class="box-title m-b-0"><a href="detail_dn.php?id=<?=$id_dn?>&cus=<?=$cu?>" class="btn btn-alt btn-sm btn-default toggle-bordered enable-tooltip" title="Kembali">Kembali</a></h3>
                         <h2><strong>Edit Data Realisasi</strong> DN</h2>
 						<br>
                         <div class="row">
                             <div class="col-md-12">
 							  <?php
 							  $id=$_GET['id'];
-							  $det=mysql_query("select * from realisasi_dn where id='$dn'")or die(mysql_error());
+							  $det=mysql_query("select * from realisasi_dn where id='$id'")or die(mysql_error());
 							  while($d=mysql_fetch_array($det)){
-							  ?>          
+							  ?>
                                 <form action="tmb_detail_dn_act.php" method="post" class="floating-labels ">
 									<div class="form-group">
 										<label>No DN</label>
 										<input name="no_dn" type="hidden" class="form-control" value="<?=$d['no_dn'] ?>"><br>
 										<p><h2><b><?=$d['no_dn'] ?></b></h2></p>
 									</div>
-									
+
 									<div class="form-group m-b-40">
 										<input name="tanggal" type="text" class="form-control mydatepicker" id="tanggal" value="<?=$d['tanggal'] ?>"  required><span class="highlight"></span> <span class="bar"></span>
 										<label for="tanggal">Tanggal</label>
@@ -76,7 +76,7 @@
                                     </div>
 									<input name="cus" type="hidden" class="form-control" value="<?=$cu?>">
 									<input name="id" type="hidden" class="form-control" value="<?=$dn?>">
-									
+
 									<div class="form-group form-actions">
 										<div class="col-md-9 col-md-offset-3">
 											<button name="submit" type="submit" class="btn btn-sm btn-primary" value="Update"><i class="fa fa-angle-right"></i> Update</button>
